1800426386c075eb873860be67fa7e4a73e7dffc
bitfountain-student <bitfountain@slowchat.us>
2015-03-20 15:50:34 -0700

    completed: Hooking up our View Objects

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var messageLabel: UILabel!
+  @IBOutlet weak var enterNameTextField: UITextField!
+  @IBOutlet weak var enterMessageTextField: UITextField!
+
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,9 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func sendMailButonPressed(sender: UIButton) {
+    
+  }
 
 }
 
